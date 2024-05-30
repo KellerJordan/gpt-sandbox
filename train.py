43a0dbe71ecfed75859e15a51ba1f0b659ac3d48
KellerJordan <kellerjordan1998@gmail.com>
2024-05-30 10:11:34 +0000

    .

--- train.py
+++ train.py
@@ -1,19 +1,5 @@
 """
-This training script can be run both on a single gpu in debug mode,
-and also in a larger training run with distributed data parallel (ddp).
-
-To run on a single GPU, example:
-$ python train.py --batch_size=32 --compile=False
-
-To run with DDP on 4 gpus on 1 node, example:
-$ torchrun --standalone --nproc_per_node=4 train.py
-
-To run with DDP on 4 gpus across 2 nodes, example:
-- Run on the first (master) node with example IP 123.456.123.456:
-$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=0 --master_addr=123.456.123.456 --master_port=1234 train.py
-- Run on the worker node:
-$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr=123.456.123.456 --master_port=1234 train.py
-(If your cluster does not have Infiniband interconnect prepend NCCL_IB_DISABLE=1)
+Should get ~2.85 loss
 """
 
 import os
@@ -214,14 +200,10 @@ while True:
         logits, loss = model(X, Y)
     # immediately async prefetch next batch while model is doing the forward pass on the GPU
     X, Y = get_batch('train')
-    # backward pass
     loss.backward()
-    # clip the gradient
     if grad_clip != 0.0:
         torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
-    # step the optimizer
     optimizer.step()
-    # flush the gradients as soon as we can, no need for this memory anymore
     optimizer.zero_grad(set_to_none=True)
 
     # timing and logging
@@ -244,3 +226,4 @@ while True:
         break
 
 destroy_process_group()
+
