1099cc3a08c1e0b524f439578ee4e4304a1f85f9
KellerJordan <kellerjordan1998@gmail.com>
2024-05-30 14:16:33 +0000

    .

--- run2.sh
+++ run2.sh
@@ -6,11 +6,11 @@ torchrun --standalone --nproc_per_node=8 base_train_gpt2.py \
     --batch_size 64 \
     --sequence_length 1024 \
     --total_batch_size 524288 \
-    --val_loss_every 953 \
-    --num_iterations 9536 \
+    --val_loss_every 128 \
+    --num_iterations 2048 \
     --weight_decay 0.1 \
     --zero_stage 1 \
     --learning_rate 0.0006 \
-    --warmup_iters 500 \
+    --warmup_iters 150 \
     --learning_rate_decay_frac 0.0 \
     --overfit_single_batch 0
