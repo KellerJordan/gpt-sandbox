96909b134a5edba6016273a95d87ac9600785107
KellerJordan <kellerjordan1998@gmail.com>
2024-06-01 00:48:44 +0000

    .

--- bench_run.sh
+++ bench_run.sh
@@ -10,5 +10,4 @@ torchrun --standalone --nproc_per_node=8 train_gpt2.py \
     --num_iterations 2048 \
     --weight_decay 0.1 \
     --learning_rate 0.0020 \
-    --warmup_iters 256 \
-    --overfit_single_batch 0
+    --warmup_iters 256
