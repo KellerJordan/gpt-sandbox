16bc447071b8c98dd23250e30f90ad29529b498e
Keller Jordan <kellerjordan1998@gmail.com>
2024-06-12 16:37:49 -0700

    .

--- run_match_gpt2.sh
+++ run_match_gpt2.sh
@@ -5,10 +5,9 @@ torchrun --standalone --nproc_per_node=8 train_gpt2.py \
     --model d12 \
     --batch_size 64 \
     --sequence_length 1024 \
-    --total_batch_size 524288 \
     --val_loss_every 128 \
     --num_iterations 9536 \
     --weight_decay 0.1 \
     --learning_rate 0.0018 \
     --warmup_iters 256 \
-    --warmdown_iters 4096
+    --warmdown_iters 2048
