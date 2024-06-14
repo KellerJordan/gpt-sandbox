f5f024d2e9198d4432c23628a304f82e218ff18e
Keller Jordan <kellerjordan1998@gmail.com>
2024-06-14 00:54:54 -0700

    .

--- bench_run.sh
+++ bench_run.sh
@@ -5,10 +5,9 @@ torchrun --standalone --nproc_per_node=8 train_gpt2.py \
     --model d12 \
     --batch_size 32 \
     --sequence_length 1024 \
-    --total_batch_size 262144 \
     --val_loss_every 128 \
     --num_iterations 2048 \
     --weight_decay 0.1 \
-    --learning_rate 0.0020 \
+    --learning_rate 0.0018 \
     --warmup_iters 256 \
     --warmdown_iters 512
